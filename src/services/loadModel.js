d863007b27c04182f2eb2cbd92b1433c155c6e0e
Muhammad Arkan Raihan <121001016+markanraihan@users.noreply.github.com>
2024-12-08 16:54:55 +0700

    Update loadModel.js

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,5 +1,5 @@
 const tf = require('@tensorflow/tfjs-node');
 async function loadModel() {
-    return tf.loadGraphModel("https://storage.cloud.google.com/ark-models-bucket/model-in-prod/model.json");
+    return tf.loadGraphModel("hhttps://storage.googleapis.com/ark-models-bucket/model-in-prod/model.json");
 }
 module.exports = loadModel;
